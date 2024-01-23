2f88b8d0ff89a0d1864bdc48e2331939514f166b
Sahak <eversuperskinny@gmail.com>
2024-01-23 20:46:09 +0400

    creating folder structure,connecting to db

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsController } from './comments/comments.controller';
+import { CommentsService } from './comments/comments.service';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule,
+    PostsModule,
+    CommentsModule,
+  ],
+  controllers: [AppController, CommentsController],
+  providers: [AppService, CommentsService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use(cookieParser());
+  app.use(cors());
+  await app.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+  });
 }
 bootstrap();
